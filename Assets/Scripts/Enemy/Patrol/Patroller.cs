11a3a3367caa25439709fdda09ba5c88c761ce7f
ERisunkov <6290744@gmail.com>
2026-05-01 21:55:49 +0200

    Edited enemy movement

--- Assets/Scripts/Enemy/Patrol/Patroller.cs
+++ Assets/Scripts/Enemy/Patrol/Patroller.cs
@@ -1,84 +1,82 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
 public class Patroller : MonoBehaviour
 {
     [SerializeField] private Waypoint[] _waypoints;
     [SerializeField] private float _speed;
-
+    
     private Vector2 _destinationPoint;
-    private bool _isMoving;
+    private WaitForSeconds _waitForSeconds;
+    private int _secondsToWait = 2;
     private int _currentDestinationPointIndex;
-    private Direction _currentDirection;
-
     private float _minimalDistanceToChangeWaypoint = 0.01f;
+    private bool _isPatrolling;
 
-    public Direction CurrentDirection => _currentDirection;
-    public bool IsMoving => _isMoving;
-
-    private void OnEnable()
+    public Action<Direction> Moving;
+    public Action Stopped;
+    
+    private void Start()
     {
-        _isMoving = true;
-
-        SetStartPoint();
-    }
-
-    private void Update()
-    {
-        if (_isMoving)
-        {
-            Patrol();
-        }
-    }
-
-    private void Patrol()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, _destinationPoint, _speed * Time.deltaTime);
-
-        if (IsDestinationPointReached())
-        {
-            SetNextDestinationPoint();
-        }
-    }
-
-    private void SetDestinationPoint(int index)
-    {
-        _currentDestinationPointIndex = index;
-
-        _destinationPoint = _waypoints[index].transform.position;
+        _waitForSeconds = new WaitForSeconds(_secondsToWait);
         
-        SetCurrentDirectionTo(_destinationPoint);
+        _isPatrolling = true;
+
+        _destinationPoint = NextPoint();
     }
 
-    private void SetNextDestinationPoint()
+    private void FixedUpdate()
     {
-        SetDestinationPoint((_currentDestinationPointIndex + 1) % _waypoints.Length);
+        if (_isPatrolling)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _destinationPoint, _speed * Time.deltaTime);
+            
+            if (IsReached(_destinationPoint))
+            {
+                StartCoroutine(SetNextPoint());
+            }
+        }
+    }
+    
+    private Vector2 NextPoint()
+    {
+        _currentDestinationPointIndex = (_currentDestinationPointIndex + 1) % _waypoints.Length;
+        
+        Vector2 nextDestinationPoint = _waypoints[_currentDestinationPointIndex].transform.position;
+        
+        NotifyMovingTo(nextDestinationPoint);
+            
+        return nextDestinationPoint;
     }
 
-    private void SetStartPoint()
+    private IEnumerator SetNextPoint()
     {
-        SetDestinationPoint(Random.Range(0, _waypoints.Length));
+        _isPatrolling = false;
+        
+        NotifyStopped();
+        
+        yield return _waitForSeconds;
+        
+        _destinationPoint = NextPoint();
+        
+        _isPatrolling = true;
     }
 
-    private bool IsDestinationPointReached()
+    private bool IsReached(Vector2 destinationPoint)
     {
-        Vector2 offset = (Vector2)transform.position - _destinationPoint;
+        Vector2 offset = (Vector2)transform.position - destinationPoint;
 
         return offset.sqrMagnitude <= _minimalDistanceToChangeWaypoint * _minimalDistanceToChangeWaypoint;
     }
 
-    private void SetCurrentDirectionTo(Vector2 direction)
+    private void NotifyMovingTo(Vector2 destinationPoint)
     {
-        if (transform.position.x < direction.x)
-        {
-            _currentDirection = Direction.Right;
-        }
-        else if (transform.position.x > direction.x)
-        {
-            _currentDirection = Direction.Left;
-        }
-        else
-        {
-            _currentDirection = Direction.None;
-        }
+        Moving?.Invoke(transform.position.x < destinationPoint.x ? Direction.Left : Direction.Right);
+    }
+
+    private void NotifyStopped()
+    {
+        Stopped?.Invoke();
     }
 }
